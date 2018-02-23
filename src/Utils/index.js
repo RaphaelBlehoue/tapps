1a8ebfea1be785b2709d114c0eb830b80d00b1c1
raphael Blehoue <raphaelsampan@gmail.com>
2018-02-23 13:00:47 +0000

    remove fonction setAuthorizationHeader.js

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,10 +1,8 @@
 import AuthParams from './AuthParams';
-import setAuthorizationHeader from './setAuthorizationHeader';
 import { TOKEN_KEY, USER_INFO } from './key';
 
 export {
     AuthParams,
-    setAuthorizationHeader,
     TOKEN_KEY,
     USER_INFO
 }
